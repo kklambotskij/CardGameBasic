82c61477f32c966426d6cf8db479e500dee6fccc
Konstantin <k-klambotsky@yandex-team.ru>
2018-08-14 23:17:30 +0700

    first commit

---
+++ Assets/Model/DeckModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class DeckModel : CardCollectionModel {
+
+    public DeckModel()
+    {
+        
+    }
+
+    protected override void Render(Vector3 position, Quaternion rotation)
+    {
+        //implement
+    }
+
+}
---
+++ Assets/Model/HandModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandModel : CardCollectionModel {
+    
+    public HandModel(string name)
+    {
+        
+    }
+
+    protected override void Render(Vector3 position, Quaternion rotation)
+    {
+        //implement
+    }
+
+}
