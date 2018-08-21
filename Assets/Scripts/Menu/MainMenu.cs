77ca136fbe0db7c800affb401923bce8328dff14
Konstantin <k-klambotsky@yandex-team.ru>
2018-08-21 11:16:48 +0700

    21.08 Артем MainMenu

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -2,44 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MainMenu : MonoBehaviour {
+public class MainMenu : MonoBehaviour
+{
 
     GameObject mCanvas;
+    //Создание листов с кнопками для меню
     List<GameObject> MainMenuButtons = new List<GameObject>();
-    List<GameObject> Options = new List<GameObject>();
-    List<GameObject> CreateGame = new List<GameObject>();
+    List<GameObject> OptionsButtons = new List<GameObject>();
+    List<GameObject> CreateGameButtons = new List<GameObject>();
+    List<GameObject> RulesButtons = new List<GameObject>();
 
 
     State mState;
 
     enum State
     {
-        MainMenu, Options, CreateGame
+        MainMenu, Options, CreateGame, Rules
     }
 
     private void UpdateState()
     {
-        DeactiveButtons(); //Выключение всех кнопок
+        //Выключение всех кнопок
+        DeactiveButtons();
 
         switch (mState)
         {
             case State.MainMenu:
-                foreach (var item in MainMenuButtons)
-                {
-                    item.SetActive(true);
-                }
+                SetActive(MainMenuButtons, true);
                 break;
             case State.Options:
-                foreach (var item in Options)
-                {
-                    item.SetActive(true);
-                }
+                SetActive(OptionsButtons, true);
                 break;
             case State.CreateGame:
-                foreach (var item in CreateGame)
-                {
-                    item.SetActive(true);
-                }
+                SetActive(CreateGameButtons, true);
                 break;
             default:
                 break;
@@ -48,13 +43,16 @@ public class MainMenu : MonoBehaviour {
 
     private void DeactiveButtons()
     {
-        foreach (var item in MainMenuButtons)
+        SetActive(MainMenuButtons, false);
+        SetActive(OptionsButtons, false);
+        SetActive(CreateGameButtons, false);
+    }
+
+    private void SetActive(List<GameObject> buttons, bool activate)
+    {
+        foreach (var item in buttons)
         {
-            item.SetActive(false);
-        }
-        foreach (var item in Options)
-        {
-            item.SetActive(false);
+            item.SetActive(activate);
         }
     }
 
@@ -70,32 +68,76 @@ public class MainMenu : MonoBehaviour {
 
         mCanvas = GameObject.Find("Canvas");
 
-        AddButton(MainMenuButtons, "NewGame");
-        AddButton(MainMenuButtons, "Options");
-        AddButton(MainMenuButtons, "Exit");
+        AddButton(MainMenuButtons, "MainMenuButtons/NewGame");
+        AddButton(MainMenuButtons, "MainMenuButtons/Options");
+        AddButton(MainMenuButtons, "MainMenuButtons/Exit");
+        OptionsButtons.Add(CreateButton("Plus", new Vector2(1500, 1000), "Menu/Button", "+", "CreateGameButtons"));
+        OptionsButtons.Add(CreateButton("InputField", new Vector2(830, 970), "Menu/InputField", "Введите имя игрока", "CreateGameButtons"));
+        OptionsButtons.Add(CreateButton("BackCreate", new Vector2(1000, 700), "Menu/Button", "Назад", "CreateGameButtons"));
+        OptionsButtons.Add(CreateButton("BackOptions", new Vector2(800, 500), "Menu/Button", "Назад", "OptionsButtons"));
+        OptionsButtons.Add(CreateButton("BackRules", new Vector2(1500, 1000), "Menu/Button", "Назад", "CreateGameButtons"));
     }
 
-    public void AddButton(List<GameObject> objects, string name)
+    private void AddButton(List<GameObject> objects, string name)
     {
-        GameObject gameObject = mCanvas.transform.Find(name).gameObject;
+        Transform transf = mCanvas.transform.Find(name);// поиск кнопки на канвасе
 
-        if (gameObject != null) //проверка на null и создание кнопки
+        if (transf != null) //проверка на null и создание кнопки
         {
-            objects.Add(gameObject);
+            objects.Add(transf.gameObject);
         }
         else
         {
-            Debug.Log("Не найдена кнопка" + name);
+            Debug.Log("Не найдена кнопка " + name); //если возвращён null
         }
     }
 
+    public GameObject CreateButton(string name, Vector2 vector, string path, string text, string parent = null)
+    {
+        GameObject gameObject = (GameObject)Instantiate(Resources.Load(path));
+        gameObject.name = name;
+        if (parent != null)
+        {
+            GameObject parentObject = mCanvas.transform.Find(parent).gameObject;
+            if (parentObject != null)
+            {
+                gameObject.transform.SetParent(parentObject.transform);
+            }
+        }
+        else
+        {
+            gameObject.transform.SetParent(mCanvas.transform);
+        }
+        gameObject.transform.position = vector;
+
+        return gameObject;
+    }
+
+    public void CreateG()
+    {
+        SwitchState(State.CreateGame);
+    }
+
+    public void Options()
+    {
+        SwitchState(State.Options);
+    }
+
+    public void Rules()
+    {
+        SwitchState(State.Rules);
+    }
+
     // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        
+    void Start()
+    {
+        UpdateState();
+        mState = State.MainMenu;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateState();
     }
 }
---
+++ Assets/Scripts/Menu/NewGame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewGame : MonoBehaviour {
+    private const string Name = "Game";
+
+    public void TaskOnClick()
+    {
+        SceneManager.LoadScene(Name);
+    }
+
+}
